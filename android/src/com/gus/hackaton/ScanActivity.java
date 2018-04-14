5d1a82674ecf41a3f965dcd756080b6fae40243f
Konrad Czarnota <k.czarnota27@gmail.com>
2018-04-14 13:43:55 +0200

    Add new activity for displaying data

--- android/src/com/gus/hackaton/ScanActivity.java
+++ android/src/com/gus/hackaton/ScanActivity.java
@@ -1,15 +1,54 @@
 package com.gus.hackaton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class ScanActivity extends AppCompatActivity
+import butterknife.ButterKnife;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+
+public class ScanActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler
 {
+    private static final String TAG = ScanActivity.class.getSimpleName();
+    private ZBarScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        ButterKnife.bind(this);
+
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(TAG, rawResult.getContents()); // Prints scan results
+        Log.v(TAG, rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
+        Intent myIntent = new Intent(ScanActivity.this, DisplayProductStatsActivity.class);
+        startActivity(myIntent);
     }
 }
