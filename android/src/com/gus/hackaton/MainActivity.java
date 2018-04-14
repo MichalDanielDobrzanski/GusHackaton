e9bf3f4d9067b93d73541fa0373640ccd66b9eb0
Konrad Czarnota <k.czarnota27@gmail.com>
2018-04-14 11:22:49 +0200

    Add new activity for scan

--- android/src/com/gus/hackaton/MainActivity.java
+++ android/src/com/gus/hackaton/MainActivity.java
@@ -1,20 +1,39 @@
 package com.gus.hackaton;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements AndroidFragmentApplication.Callbacks {
+
+	@BindView(R.id.scan_barcode)
+	Button scanBarcode;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main_activity);
+        ButterKnife.bind(this);
 
+		scanBarcode.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent myIntent = new Intent(MainActivity.this, ScanActivity.class);
+				startActivity(myIntent);
+			}
+		});
 		getSupportFragmentManager()
 				.beginTransaction()
 				.replace(R.id.heroContainer, new HeroFragment())
---
+++ android/src/com/gus/hackaton/ScanActivity.java
@@ -0,0 +1,15 @@
+package com.gus.hackaton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
