d84694e08a3eb1f568d11a24c9d60023f8a5cfd4
Konrad Czarnota <k.czarnota27@gmail.com>
2018-04-14 14:30:04 +0200

    Scanning and showing text

--- android/src/com/gus/hackaton/ScanActivity.java
+++ android/src/com/gus/hackaton/ScanActivity.java
@@ -1,10 +1,13 @@
 package com.gus.hackaton;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.cameraview.CameraView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
@@ -15,13 +18,17 @@ public class ScanActivity extends AppCompatActivity implements ZBarScannerView.R
     private static final String TAG = ScanActivity.class.getSimpleName();
     private ZBarScannerView mScannerView;
 
+    @BindView(R.id.display_view)
+    CameraView mCameraView;
+    @BindView(R.id.textview)
+    TextView textView;
+
+    private boolean scanned = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
-        ButterKnife.bind(this);
-
         mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
     }
@@ -30,25 +37,35 @@ public class ScanActivity extends AppCompatActivity implements ZBarScannerView.R
     @Override
     public void onResume() {
         super.onResume();
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();          // Start camera on resume
+        if(!scanned) {
+            mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+            mScannerView.startCamera();          // Start camera on resume
+        } else {
+            mCameraView.start();
+        }
     }
 
     @Override
     public void onPause() {
+        if(!scanned)
+            mScannerView.stopCamera();           // Stop camera on pause
+        else {
+            mCameraView.stop();
+        }
         super.onPause();
-        mScannerView.stopCamera();           // Stop camera on pause
     }
 
     @Override
     public void handleResult(Result rawResult) {
         // Do something with the result here
-        Log.v(TAG, rawResult.getContents()); // Prints scan results
-        Log.v(TAG, rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+        Log.d(TAG, rawResult.getContents()); // Prints scan results
 
         // If you would like to resume scanning, call this method below:
         //mScannerView.resumeCameraPreview(this);
-        Intent myIntent = new Intent(ScanActivity.this, DisplayProductStatsActivity.class);
-        startActivity(myIntent);
+        scanned = true;
+        setContentView(R.layout.activity_scan);
+        ButterKnife.bind(this);
+        mCameraView.start();
+        textView.setText(rawResult.getContents());
     }
 }
